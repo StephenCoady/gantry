32eb58bd6b3fd8a941f6d08b4ee0344a0fe2f83e
Stephen Coady <scoady2@gmail.com>
2017-02-16 13:43:08 +0000

    sidebar working - not currently controlled by angular

--- app/app.js
+++ app/app.js
@@ -1,14 +1,21 @@
 angular.module('uiForDocker', ['ngRoute', 'toaster', 'ngAnimate'])
-  .controller('LandingCtrl', function($scope, containerApi, imageApi, toaster, $route) {})
+  .controller('LandingCtrl', function($scope, containerApi, imageApi, toaster, $route) {
+    $scope.isActive = function (viewLocation) { 
+      console.log(viewLocation);
+    return viewLocation === $location.path();
+};
+  })
   .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
         controller: 'MainCtrl',
-        templateUrl: 'components/main/main.html'
+        templateUrl: 'components/main/main.html',
+        activetab: 'dashboard'
       })
       .when('/containers', {
         controller: 'ContainersCtrl',
-        templateUrl: 'components/containers/containers.html'
+        templateUrl: 'components/containers/containers.html',
+        activetab: 'containers'
       })
       .otherwise({
         redirectTo: '/'
--- app/components/main/mainController.js
+++ app/components/main/mainController.js
@@ -1,8 +1,11 @@
 angular.module('uiForDocker')
 .controller('MainCtrl', MainCtrl);
 
-MainCtrl.$inject = ['$scope', '$http']; 
+MainCtrl.$inject = ['$scope', '$http', '$route', '$location']; 
 
-function MainCtrl($scope, $http) {
+function MainCtrl($scope, $http, $route, $location) {
 	$scope.test = 'MAIN CONTROLLER';
+	$scope.isActive = function(route) {
+			return route === $location.path();
+	}
 }
