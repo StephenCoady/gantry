d00b7c226ee02483c9b43b21407796b24307bdd3
Stephen Coady <scoady2@gmail.com>
2017-02-27 15:14:32 +0000

    typo fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -i /tmp/stephen.key "$1" 'docker stop staging-application && \
+ssh -o "StrictHostKeyChecking no" -i /tmp/stephen.key "$1" 'docker stop staging-application && \
 docker rm staging-application && \
 docker rmi scoady2/lifecycle-management-for-docker && \
 docker run -d -p 3000:3000 --name staging-application -v /var/run/docker.sock:/var/run/docker.sock scoady2/lifecycle-management-for-docker'
