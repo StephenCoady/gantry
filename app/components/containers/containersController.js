aab991e9dc87c52e19e24de1b5e38275ce0309a9
Stephen Coady <scoady2@gmail.com>
2017-04-21 21:11:20 +0100

    hot fix for console bug

--- app/components/containers/containersController.js
+++ app/components/containers/containersController.js
@@ -1,287 +1,303 @@
 angular.module('uiForDocker')
-  .controller('ContainersCtrl', ContainersCtrl);
+	.controller('ContainersCtrl', ContainersCtrl);
 
 ContainersCtrl.$inject = ['$scope', '$http', '$location', 'containerApi', 'imageApi', 'toaster', '$route', '$filter', 'volumeApi'];
 
 function ContainersCtrl($scope, $http, $location, containerApi, imageApi, toaster, $route, $filter, volumeApi) {
-  $scope.base = ($location.$$host);
-  $scope.volumeOrPath = "path";
-  $scope.isActive = function(route) {
-    return route === $location.path();
-  }
-  
-  $scope.form = {
-    Volumes: []
-  }
-  
-  $scope.options = {
-    name: "",
-    Image: "",
-    ExposedPorts: {},
-    HostConfig: {
-      PortBindings: [],
-      Binds: [],
-      NetworkMode: "bridge",
-      Privileged: false
-    }
-  };
+	$scope.base = ($location.$$host);
+	$scope.volumeOrPath = "path";
+	$scope.isActive = function(route) {
+		return route === $location.path();
+	}
 
-  $scope.addPortBinding = function() {
-    $scope.options.HostConfig.PortBindings.push({
-      hostPort: '',
-      containerPort: '',
-      protocol: 'tcp'
-    });
-  };
+	$scope.form = {
+		Volumes: []
+	}
 
-  $scope.removePortBinding = function(index) {
-    $scope.options.HostConfig.PortBindings.splice(index, 1);
-  };
+	$scope.options = {
+		name: "",
+		Image: "",
+		ExposedPorts: {},
+		HostConfig: {
+			PortBindings: [],
+			Binds: [],
+			NetworkMode: "bridge",
+			Privileged: false
+		}
+	};
 
-  $scope.addVolume = function() {
-    $scope.form.Volumes.push({
-      name: '',
-      containerPath: ''
-    });
-  };
+	$scope.addPortBinding = function() {
+		$scope.options.HostConfig.PortBindings.push({
+			hostPort: '',
+			containerPort: '',
+			protocol: 'tcp'
+		});
+	};
 
-  $scope.removeVolume = function(index) {
-    $scope.form.Volumes.splice(index, 1);
-  };
+	$scope.removePortBinding = function(index) {
+		$scope.options.HostConfig.PortBindings.splice(index, 1);
+	};
 
-  function preparePortBindings(options) {
-    var bindings = {};
-    options.HostConfig.PortBindings.forEach(function(portBinding) {
-      if (portBinding.containerPort) {
-        var key = portBinding.containerPort + "/" + portBinding.protocol;
-        var binding = {};
-        if (portBinding.hostPort && portBinding.hostPort.indexOf(':') > -1) {
-          var hostAndPort = portBinding.hostPort.split(':');
-          binding.HostIp = hostAndPort[0];
-          binding.HostPort = hostAndPort[1];
-        } else {
-          binding.HostPort = portBinding.hostPort;
-        }
-        bindings[key] = [binding];
-        $scope.options.ExposedPorts[key] = {};
-      }
-    });
-    $scope.options.HostConfig.PortBindings = bindings;
-  };
-  
-  function prepareVolumes() {
-  var binds = [];
-  var volumes = {};
+	$scope.addVolume = function() {
+		$scope.form.Volumes.push({
+			name: '',
+			containerPath: ''
+		});
+	};
 
-  $scope.form.Volumes.forEach(function (volume) {
-    var name = volume.name;
-    var containerPath = volume.containerPath;
-    if (name && containerPath) {
-      var bind = name + ':' + containerPath;
-      volumes[containerPath] = {};
-      if (volume.readOnly) {
-        bind += ':ro';
-      }
-      binds.push(bind);
-    }
-  });
-  $scope.options.HostConfig.Binds = binds;
-  $scope.options.Volumes = volumes;
-}
+	$scope.removeVolume = function(index) {
+		$scope.form.Volumes.splice(index, 1);
+	};
 
-  $scope.createContainer = function() {
-    preparePortBindings($scope.options);
-    prepareVolumes();
-    let image = {};
-    image.name = $scope.options.Image;
-    if($scope.options.tag === undefined){
-      image.tag = 'latest'
-    }
-    else{
-      image.tag = $scope.options.tag;
-    }
-    imageApi.pull(image)
-    .then(function(response) {
-      toaster.pop('success', "Success", "Image " + $scope.options.Image + " pulled.");
-    })
-    .then(function(response) {
-      containerApi.create($scope.options)
-        .then(function(response) {
-          toaster.pop('success', "Success", "Container " + $scope.options.name + " created.");
-          $route.reload();
-          return response;
-        })
-        .then(function(response) {
-          containerApi.start(response.data.data.id)
-            .then(function(response) {
-              toaster.pop('success', "Success", "Container " + $scope.options.name + " started.");
-              $route.reload();
-            })
-            .catch(function(e) {
-              toaster.pop('error', "Error", "Container " + $scope.options.name + " cannot be started.");
-            })
-        })
-    })
-    .catch(function(e) {
-      toaster.pop('error', "Error", "Image " + $scope.options.Image + " cannot be pulled. Is the account named included?");
-    })
-  };
-  
-  volumeApi.getAll().then(function(response) {
+	function preparePortBindings(options) {
+		var bindings = {};
+		options.HostConfig.PortBindings.forEach(function(portBinding) {
+			if (portBinding.containerPort) {
+				var key = portBinding.containerPort + "/" + portBinding.protocol;
+				var binding = {};
+				if (portBinding.hostPort && portBinding.hostPort.indexOf(':') > -1) {
+					var hostAndPort = portBinding.hostPort.split(':');
+					binding.HostIp = hostAndPort[0];
+					binding.HostPort = hostAndPort[1];
+				} else {
+					binding.HostPort = portBinding.hostPort;
+				}
+				bindings[key] = [binding];
+				$scope.options.ExposedPorts[key] = {};
+			}
+		});
+		$scope.options.HostConfig.PortBindings = bindings;
+	};
+
+	function prepareConsole() {
+		var value = $scope.formValues.Console;
+		var openStdin = true;
+		var tty = true;
+		if (value === 'tty') {
+			openStdin = false;
+		} else if (value === 'interactive') {
+			tty = false;
+		} else if (value === 'none') {
+			openStdin = false;
+			tty = false;
+		}
+		$scope.options.OpenStdin = openStdin;
+		$scope.options.Tty = tty;
+	}
+
+	function prepareVolumes() {
+		var binds = [];
+		var volumes = {};
+
+		$scope.form.Volumes.forEach(function(volume) {
+			var name = volume.name;
+			var containerPath = volume.containerPath;
+			if (name && containerPath) {
+				var bind = name + ':' + containerPath;
+				volumes[containerPath] = {};
+				if (volume.readOnly) {
+					bind += ':ro';
+				}
+				binds.push(bind);
+			}
+		});
+		$scope.options.HostConfig.Binds = binds;
+		$scope.options.Volumes = volumes;
+	}
+
+	$scope.createContainer = function() {
+		preparePortBindings($scope.options);
+		prepareVolumes();
+    prepareConsole();
+		let image = {};
+		image.name = $scope.options.Image;
+		if ($scope.options.tag === undefined) {
+			image.tag = 'latest'
+		} else {
+			image.tag = $scope.options.tag;
+		}
+		imageApi.pull(image)
+			.then(function(response) {
+				toaster.pop('success', "Success", "Image " + $scope.options.Image + " pulled.");
+			})
+			.then(function(response) {
+				containerApi.create($scope.options)
+					.then(function(response) {
+						toaster.pop('success', "Success", "Container " + $scope.options.name + " created.");
+						$route.reload();
+						return response;
+					})
+					.then(function(response) {
+						containerApi.start(response.data.data.id)
+							.then(function(response) {
+								toaster.pop('success', "Success", "Container " + $scope.options.name + " started.");
+								$route.reload();
+							})
+							.catch(function(e) {
+								toaster.pop('error', "Error", "Container " + $scope.options.name + " cannot be started.");
+							})
+					})
+			})
+			.catch(function(e) {
+				toaster.pop('error', "Error", "Image " + $scope.options.Image + " cannot be pulled. Is the account named included?");
+			})
+	};
+
+	volumeApi.getAll().then(function(response) {
 		$scope.volumes = response.data.volumes.Volumes;
 	}, function(error) {});
 
-  containerApi.getAll().then(function(response) {
-    $scope.containers = response.data.containers;
-    $scope.currentPage = 0;
-    $scope.pageSize = 10;
-    $scope.q = '';
-    $scope.getData = function() {
-      // needed for the pagination calc
-      // https://docs.angularjs.org/api/ng/filter/filter
-      return $filter('filter')($scope.containers, $scope.q)
-    }
-    $scope.numberOfPages = function() {
-      let pages = Math.ceil($scope.getData().length / $scope.pageSize);
-      if(pages){
-        return pages;
-      } else {
-        return 1;
-      }
-    }
+	containerApi.getAll().then(function(response) {
+		$scope.containers = response.data.containers;
+		$scope.currentPage = 0;
+		$scope.pageSize = 10;
+		$scope.q = '';
+		$scope.getData = function() {
+			// needed for the pagination calc
+			// https://docs.angularjs.org/api/ng/filter/filter
+			return $filter('filter')($scope.containers, $scope.q)
+		}
+		$scope.numberOfPages = function() {
+			let pages = Math.ceil($scope.getData().length / $scope.pageSize);
+			if (pages) {
+				return pages;
+			} else {
+				return 1;
+			}
+		}
 
-    $scope.$watch('q', function(newValue, oldValue) {
-      if (oldValue != newValue) {
-        $scope.currentPage = 0;
-      }
-    }, true);
+		$scope.$watch('q', function(newValue, oldValue) {
+			if (oldValue != newValue) {
+				$scope.currentPage = 0;
+			}
+		}, true);
 
-  }, function(error) {});
+	}, function(error) {});
 
 
-  $scope.checkAll = function() {
-    angular.forEach($scope.containers, function(container) {
-      container.select = $scope.selectAll;
-    });
-  };
+	$scope.checkAll = function() {
+		angular.forEach($scope.containers, function(container) {
+			container.select = $scope.selectAll;
+		});
+	};
 
-  $scope.start = function(containers) {
-    for (var i = 0; i < containers.length; i++) {
-      if (containers[i].select) {
-        var container = containers[i];
-        startHandler(container);
-      }
-    }
-  }
+	$scope.start = function(containers) {
+		for (var i = 0; i < containers.length; i++) {
+			if (containers[i].select) {
+				var container = containers[i];
+				startHandler(container);
+			}
+		}
+	}
 
-  $scope.stop = function(containers) {
-    for (var i = 0; i < containers.length; i++) {
-      if (containers[i].select) {
-        var container = containers[i];
-        stopHandler(container);
-      }
-    }
-  }
+	$scope.stop = function(containers) {
+		for (var i = 0; i < containers.length; i++) {
+			if (containers[i].select) {
+				var container = containers[i];
+				stopHandler(container);
+			}
+		}
+	}
 
-  $scope.pause = function(containers) {
-    for (var i = 0; i < containers.length; i++) {
-      if (containers[i].select) {
-        var container = containers[i];
-        pauseHandler(container);
-      }
-    }
-  }
+	$scope.pause = function(containers) {
+		for (var i = 0; i < containers.length; i++) {
+			if (containers[i].select) {
+				var container = containers[i];
+				pauseHandler(container);
+			}
+		}
+	}
 
-  $scope.unpause = function(containers) {
-    for (var i = 0; i < containers.length; i++) {
-      if (containers[i].select) {
-        var container = containers[i];
-        unpauseHandler(container);
-      }
-    }
-  }
+	$scope.unpause = function(containers) {
+		for (var i = 0; i < containers.length; i++) {
+			if (containers[i].select) {
+				var container = containers[i];
+				unpauseHandler(container);
+			}
+		}
+	}
 
-  $scope.restart = function(containers) {
-    for (var i = 0; i < containers.length; i++) {
-      if (containers[i].select) {
-        var container = containers[i];
-        restartHandler(container);
-      }
-    }
-  }
+	$scope.restart = function(containers) {
+		for (var i = 0; i < containers.length; i++) {
+			if (containers[i].select) {
+				var container = containers[i];
+				restartHandler(container);
+			}
+		}
+	}
 
-  $scope.remove = function(containers) {
-    for (var i = 0; i < containers.length; i++) {
-      if (containers[i].select) {
-        var container = containers[i];
-        removeHandler(container);
-      }
-    }
-  }
+	$scope.remove = function(containers) {
+		for (var i = 0; i < containers.length; i++) {
+			if (containers[i].select) {
+				var container = containers[i];
+				removeHandler(container);
+			}
+		}
+	}
 
-  function startHandler(container) {
-    containerApi.start(container.Id)
-      .then(function(response) {
-        toaster.pop('success', "Success", "Container " + container.Names[0] + " started.");
-        $route.reload();
-      })
-      .catch(function(e) {
-        toaster.pop('error', "Error", "Container " + container.Names[0] + " cannot be started.");
-      })
-  }
+	function startHandler(container) {
+		containerApi.start(container.Id)
+			.then(function(response) {
+				toaster.pop('success', "Success", "Container " + container.Names[0] + " started.");
+				$route.reload();
+			})
+			.catch(function(e) {
+				toaster.pop('error', "Error", "Container " + container.Names[0] + " cannot be started.");
+			})
+	}
 
-  function stopHandler(container) {
-    containerApi.stop(container.Id)
-      .then(function(response) {
-        toaster.pop('success', "Success", "Container " + container.Names[0] + " stopped.");
-        $route.reload();
-      })
-      .catch(function(e) {
-        toaster.pop('error', "Error", "Container " + container.Names[0] + " cannot be stopped.");
-      })
-  }
+	function stopHandler(container) {
+		containerApi.stop(container.Id)
+			.then(function(response) {
+				toaster.pop('success', "Success", "Container " + container.Names[0] + " stopped.");
+				$route.reload();
+			})
+			.catch(function(e) {
+				toaster.pop('error', "Error", "Container " + container.Names[0] + " cannot be stopped.");
+			})
+	}
 
-  function pauseHandler(container) {
-    containerApi.pause(container.Id)
-      .then(function(response) {
-        toaster.pop('success', "Success", "Container " + container.Names[0] + " paused.");
-        $route.reload();
-      })
-      .catch(function(e) {
-        toaster.pop('error', "Error", "Container " + container.Names[0] + " cannot be paused.");
-      })
-  }
+	function pauseHandler(container) {
+		containerApi.pause(container.Id)
+			.then(function(response) {
+				toaster.pop('success', "Success", "Container " + container.Names[0] + " paused.");
+				$route.reload();
+			})
+			.catch(function(e) {
+				toaster.pop('error', "Error", "Container " + container.Names[0] + " cannot be paused.");
+			})
+	}
 
-  function unpauseHandler(container) {
-    containerApi.unpause(container.Id)
-      .then(function(response) {
-        toaster.pop('success', "Success", "Container " + container.Names[0] + " unpaused.");
-        $route.reload();
-      })
-      .catch(function(e) {
-        toaster.pop('error', "Error", "Container " + container.Names[0] + " cannot be unpaused.");
-      })
-  }
+	function unpauseHandler(container) {
+		containerApi.unpause(container.Id)
+			.then(function(response) {
+				toaster.pop('success', "Success", "Container " + container.Names[0] + " unpaused.");
+				$route.reload();
+			})
+			.catch(function(e) {
+				toaster.pop('error', "Error", "Container " + container.Names[0] + " cannot be unpaused.");
+			})
+	}
 
-  function restartHandler(container) {
-    containerApi.restart(container.Id)
-      .then(function(response) {
-        toaster.pop('success', "Success", "Container " + container.Names[0] + " restarted.");
-        $route.reload();
-      })
-      .catch(function(e) {
-        toaster.pop('error', "Error", "Container " + container.Names[0] + " cannot be restarted.");
-      })
-  }
+	function restartHandler(container) {
+		containerApi.restart(container.Id)
+			.then(function(response) {
+				toaster.pop('success', "Success", "Container " + container.Names[0] + " restarted.");
+				$route.reload();
+			})
+			.catch(function(e) {
+				toaster.pop('error', "Error", "Container " + container.Names[0] + " cannot be restarted.");
+			})
+	}
 
-  function removeHandler(container) {
-    containerApi.remove(container.Id)
-      .then(function(response) {
-        toaster.pop('success', "Success", "Container " + container.Names[0] + " removed.");
-        $route.reload();
-      })
-      .catch(function(e) {
-        toaster.pop('error', "Error", "Container " + container.Names[0] + " cannot be removed. Is it stopped?");
-      })
-  }
+	function removeHandler(container) {
+		containerApi.remove(container.Id)
+			.then(function(response) {
+				toaster.pop('success', "Success", "Container " + container.Names[0] + " removed.");
+				$route.reload();
+			})
+			.catch(function(e) {
+				toaster.pop('error', "Error", "Container " + container.Names[0] + " cannot be removed. Is it stopped?");
+			})
+	}
 }
